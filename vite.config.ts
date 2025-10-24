86d589d6c6fa87107babef540f40f768ee3d2647
Ewerton Gomes Lisboa <149094494+ewertongomeslisboa@users.noreply.github.com>
2025-10-24 12:06:59 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ewertongomeslisboa.io/', // <- ESSENCIAL para funcionar no Netlify e GitHub Pages
+  base: '/ecovibe/',
 })
